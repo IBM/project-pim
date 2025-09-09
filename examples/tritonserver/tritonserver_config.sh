57262c24ed2f143ddc3e6c7b5ff5a2ebc944ee29
Manjunath-A-C <manjunath.a.c@ibm.com>
2025-09-09 11:07:59 +0530

    refactor model repository path to store different models
    Signed-off-by: Manjunath-A-C <manjunath.a.c@ibm.com>

--- examples/tritonserver/tritonserver_config.sh
+++ examples/tritonserver/tritonserver_config.sh
@@ -2,23 +2,24 @@
 
 set -x
 
-mkdir -p /etc/pim/models/fraud_detection
+[ -f /etc/pim/tritonserver.conf ] || touch /etc/pim/tritonserver.conf
 
-[ -f /etc/pim/fraud_detection.conf ] || touch /etc/pim/fraud_detection.conf
+# List of AI applications to be served from tritonserver
+ai_apps=("fraud")
 
-mkdir -p /var/models/fraud_detection
-mkdir -p /var/models/fraud_detection/model_repository/fraud/1
+for app in "${ai_apps[@]}"; do
+    mkdir -p /var/models/model_repository/${app}/1
 
-ONNX_MODEL_SOURCE=$(jq -r '.modelSource' /etc/pim/pim_config.json)
-# Download model from the http server
-if [[ -n "$ONNX_MODEL_SOURCE" ]]; then
-   curl "$ONNX_MODEL_SOURCE" --output /var/models/fraud_detection/model_repository/fraud/1/model.onnx
-fi
+    ONNX_MODEL_SOURCE=$(jq -r '.modelSource' /etc/pim/pim_config.json)
+    if [[ -n "$ONNX_MODEL_SOURCE" ]]; then
+    curl "$ONNX_MODEL_SOURCE" --output /var/models/model_repository/${app}/1/model.onnx
+    fi
 
-CONFIG_FILE=$(jq -r '.configSource' /etc/pim/pim_config.json)
-if [[ -n "$CONFIG_FILE" ]]; then
-    curl "$CONFIG_FILE" --output /var/models/fraud_detection/model_repository/fraud/config.pbtxt
-fi
+    CONFIG_FILE=$(jq -r '.configSource' /etc/pim/pim_config.json)
+    if [[ -n "$CONFIG_FILE" ]]; then
+        curl "$CONFIG_FILE" --output /var/models/model_repository/${app}/config.pbtxt
+    fi
+done
 
-var_to_add=MODEL_REPOSITORY=/var/models/fraud_detection/model_repository
-sed -i "/^MODEL_REPOSITORY=.*/d"  /etc/pim/fraud_detection.conf && echo "$var_to_add" >> /etc/pim/fraud_detection.conf
+var_to_add=MODEL_REPOSITORY=/var/models/model_repository
+sed -i "/^MODEL_REPOSITORY=.*/d"  /etc/pim/tritonserver.conf && echo "$var_to_add" >> /etc/pim/tritonserver.conf
